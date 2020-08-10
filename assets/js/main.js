3284ecabac0f5650a02c5912ce119dfdc859399e
Julianto08 <jjulianto08@gmail.com>
2020-08-10 17:28:32 +0700

    Added index.html, style.css, images, and main.js

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+function searchKota() {
+    let input = document.getElementById("search");
+    let content = document.getElementById("content");
+    let article = content.getElementsByTagName("article");
+    filter = input.value.toUpperCase();
+
+    if (filter == '') {
+        for (i = 0; i < article.length; i++) {
+            article[i].style.display = "";
+            if (i == 0) {
+                article[i].classList.add("first");
+                article[i].classList.add("top");
+            } else {
+                article[i].classList.remove("first");
+            }
+        }
+    } else {
+        for (i = 0; i < article.length; i++) {
+            a = article[i].getElementsByTagName("h2")[0];
+            txtValue = a.textContent || a.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                article[i].style.display = "";
+                article[i].classList.add("first");
+                article[i].classList.remove("top");
+            } else {
+                article[i].style.display = "none";
+            }
+        }
+    }
+}
